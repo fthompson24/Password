a32c29c50a81a931a43d1db06640884be1615b9e
fthompson24 <fraserjthompson@gmail.com>
2021-01-11 21:02:07 -0600

    random pass populates in console - cant get it in text box

--- Develop/script.js
+++ Develop/script.js
@@ -7,22 +7,55 @@ let passCriteria = [];
 
 //Prompts for passoword criteria
 function generatePassword () {
-  let passwordString="";
+  let pass="";
   alert("Please set the criteria for your passoword.");
   let setUpperCase=confirm("Include ~UPPERCASE~ letters?");
   let setLowerCase=confirm("Include ~lowercase~ letter?");
   let setNumbers=confirm("Include numbers?");
-  let useSymbols=confirm("Include special character? Eg '!#%^' ");
+  let setSymbols=confirm("Include special character? Eg '!#%^' ");
 
-  
+  //added selected characters to password array
+  if (setUpperCase===true){
+    passCriteria.push(upperCase);
+  }
+  if (setLowerCase===true){
+    passCriteria.push(lowerCase);
+  }
+  if (setNumbers===true) {
+    passCriteria.push(numbers);
+  }
+  if (setSymbols===true) {
+    passCriteria.push(symbols)
+  }
 
 
   //Prompt for passowrd length
-  let passLength = Number(prompt("How many characters will your passoword be? Enter a number betweem 8-128."));
+  let passLength =parseInt(prompt("How many characters will your passoword be? Enter a number betweem 8-128."));
   while(passLength < 8 || passLength > 128) {
     alert("Sorry, your password must be betwen 8-128 characters.");
     passLength = Number(prompt("How many characters will your passoword be? Enter a number betweem 8-128."));
   }
+
+  //
+  for(let i=0;i<passLength;i++){
+
+    let randomCharSet       //variable that store random number based on length of passCriteria[]
+    let selectedCharSet     //variable that stores selected pass criteria
+    let randomCharNumber    //variable that stores random number based on length of pass crit.
+    let randomPassword
+     
+    randomCharSet = parseInt(Math.floor(Math.random()*passCriteria.length));
+
+    selectedCharSet = passCriteria[randomCharSet];
+
+    randomCharNumber = Math.floor(Math.random()*selectedCharSet.length);
+
+    randomPassword = selectedCharSet[randomCharNumber];
+
+    //add to password string
+    pass+=randomPassword
+  }
+  console.log("Password --- " + pass);
 }
 
 
@@ -35,7 +68,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
